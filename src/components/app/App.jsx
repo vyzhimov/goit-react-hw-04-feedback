57bd2d3daa3d7f1305ff6546e014f11e9072e92f
Alexander Vyzhymov <vizimov@gmail.com>
2023-04-30 10:09:26 +0300

    fixed errors

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -14,13 +14,13 @@ export default function App() {
   const handleFeedbackBtnClick = option => {
     switch (option) {
       case 'good':
-        setGood(good + 1);
+        setGood(prev => prev + 1);
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prev => prev + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(prev => prev + 1);
         break;
       default:
         return '';
